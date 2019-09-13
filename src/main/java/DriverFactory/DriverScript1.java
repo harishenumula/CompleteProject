68b8fc9df32fc00965851a8c512b367d50f9e17e
harishenumula <harishenumula5@gmail.com>
2019-09-14 00:10:29 +0530

    extend reports added

--- src/main/java/DriverFactory/DriverScript1.java
+++ src/main/java/DriverFactory/DriverScript1.java
@@ -5,7 +5,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 import CommonFunLibrary.FunctionLibrary;
 import Utilities.ExcelFileUtil;
@@ -14,6 +20,32 @@ public class DriverScript1 {
 	WebDriver driver;
 	String step_status;
 	String testcase_status;
+	ExtentReports extent;
+	ExtentTest logger;
+
+	@BeforeTest
+	public void setExtent() {
+		extent = new ExtentReports(System.getProperty("user.dir") + "\\test-output\\ExtentReport.html", true);
+		System.out.println("creating extent report at \n"+System.getProperty("user.dir") + "\\test-output\\ExtentReport.html);
+		extent.addSystemInfo("username", "Harish");
+		extent.addSystemInfo("OS", "Windows");
+		extent.addSystemInfo("env", "testing");
+
+	}
+
+	@AfterTest
+	public void endreport() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		extent.flush();
+		extent.close();
+		driver.close();
+
+	}
 
 	@Test
 	public void start_Test() throws Throwable, Throwable {
@@ -23,7 +55,10 @@ public class DriverScript1 {
 		for (int i = 1; i <= no_test_cases; i++) {
 			String execution_mode = xls.getData("Dashboard", i, 2);
 			if (execution_mode.equalsIgnoreCase("y")) {
+
 				String test_case_name = xls.getData("Dashboard", i, 1);
+				logger = extent.startTest(test_case_name);
+
 				int no_of_steps = xls.getrowCount(test_case_name);
 				for (int j = 1; j <= no_of_steps; j++) {
 					String description = xls.getData(test_case_name, j, 0);
@@ -63,13 +98,17 @@ public class DriverScript1 {
 					System.out.println(description + ":\t" + step_status);
 					if (testcase_status.equalsIgnoreCase("true")) {
 						xls.setData("Dashboard", i, 3, "pass");
+						logger.log(LogStatus.PASS, description);
 					} else {
 						xls.setData("Dashboard", i, 3, "fail");
+						logger.log(LogStatus.FAIL, description);
 					}
 				}
+				extent.endTest(logger);
 			} else {
 				xls.setData("Dashboard", i, 3, "Not Executed");
 			}
 		}
 	}
+
 }
